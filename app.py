eb82ed082e229404758fce9fb2fe63bc6489ac38
TLodosDurak <tlodosdurak@gmail.com>
2023-05-09 18:40:02 -0400

    New PromptTemplate, Layout and extra content

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ import json
 import random
 from dotenv import load_dotenv
 import streamlit as st
+from av import AVError
+from moviepy.editor import ImageSequenceClip
+from PIL import Image
+
 from moviepy.config import change_settings
 
 change_settings(
@@ -41,7 +45,7 @@ def delete_media_files(num_files: int) -> None:
 
 def create_title_card(title_text, duration=4, bg_video=None):
     title_clip = TextClip(title_text, fontsize=45, font="Arial-Bold",
-                          color='YellowGreen', size=(540, 960), stroke_color='black', stroke_width=2)
+                          color='YellowGreen', size=(540, 960), method='caption', align='center', stroke_color='black', stroke_width=2)
     title_clip = title_clip.set_duration(duration)
 
     if bg_video is not None:
@@ -54,44 +58,54 @@ def create_title_card(title_text, duration=4, bg_video=None):
     return title_card
 
 
-def create_ranking_frame(rank, text, duration=2, bg_video=None, media_file_path=None):
-    canvas_clip = TextClip(f"Canvas", fontsize=45, font="Arial-Bold",
-                        color='YellowGreen', size=(540, 960), stroke_color='black', stroke_width=2)
-    txt_clip = TextClip(f"Rank {rank}: {text}", fontsize=45, font="Arial-Bold",
-                        color='YellowGreen', size=(540, 960), stroke_color='black', stroke_width=2)
-    canvas_clip = txt_clip.set_duration(duration)
-    txt_clip = txt_clip.set_duration(duration)
+def create_ranking_frame(rank, country, why, what, duration=2, bg_video=None, media_file_path=None):
+    canvas_clip = TextClip(f" ", fontsize=45, font="Arial-Bold",
+                        color='White', size=(540, 960), stroke_color='black', stroke_width=2, transparent= True)
+    txt_clip = TextClip(f"Rank {rank}: {country}", fontsize=45, font="Arial-Bold",
+                        color='YellowGreen', size=(540, 960), method='caption', align='center', stroke_color='black', stroke_width=2)
+    why_clip = TextClip(f"{why}", fontsize=35, font="Arial-Bold",
+                        color='white', size=(540, 960), method='caption', align='center', stroke_color='black', stroke_width=2)
+    what_clip = TextClip(f"{what}", fontsize=35, font="Arial-Bold",
+                        color='white', size=(540, 960), method='caption', align='center', stroke_color='black', stroke_width=2)
 
+    canvas_clip = canvas_clip.set_duration(duration)
+    txt_clip = txt_clip.set_position(('center', 50)).set_duration(duration)
+    why_clip = why_clip.set_position(('center', 250)).set_duration(duration)
+    what_clip = what_clip.set_position(('center', 300)).set_duration(duration)
     if bg_video is not None:
         # Resize the background video to fit 9:16 aspect ratio
         bg_video = bg_video.resize(height=960, width=540)
         bg_video = bg_video.set_duration(duration)
 
     if media_file_path is not None:
-        media_clip = ImageClip(media_file_path).set_duration(duration)
-        # Resize the media clip as needed
-        media_clip = media_clip.resize(height=300)
-        # Position the media clip on the frame
-        media_clip = media_clip.set_position(("center"))
+        try:
+            media_clip = ImageClip(media_file_path).set_duration(duration)
+            # Resize the media clip as needed
+            media_clip = media_clip.resize(height=300)
+            # Position the media clip on the frame
+            media_clip = media_clip.set_position(("center", 960 - media_clip.h - 550))
+        except AVError:
+            print(f"Error processing the media file: {media_file_path}")
+            media_clip = None
 
         if bg_video is not None:
-            clip = CompositeVideoClip([canvas_clip, bg_video, media_clip, txt_clip])
+            clip = CompositeVideoClip([canvas_clip, bg_video, media_clip, txt_clip, why_clip, what_clip])
         else:
-            clip = CompositeVideoClip([canvas_clip, media_clip, txt_clip])
+            clip = CompositeVideoClip([canvas_clip, media_clip, txt_clip, why_clip, what_clip])
     else:
         if bg_video is not None:
-            clip = CompositeVideoClip([canvas_clip, bg_video, txt_clip])
+            clip = CompositeVideoClip([canvas_clip, bg_video, txt_clip, why_clip, what_clip])
         else:
-            clip = txt_clip
+            clip = CompositeVideoClip([txt_clip, why_clip, what_clip])
 
     return clip
 
 
-def create_video(ranking_list, user_input, bg_videos=None, bg_music=None):
+def create_video(ranking_list, topic, bg_videos=None, bg_music=None):
     clips = []
     j = 0
     # Title card
-    title_text = f"Top 10 {user_input}"
+    title_text = f"Top 10 Countries: {topic}"
     if bg_videos is not None:
         bg_video = bg_videos[j % len(bg_videos)] if bg_videos else None
     else:
@@ -99,23 +113,31 @@ def create_video(ranking_list, user_input, bg_videos=None, bg_music=None):
     clips.append(create_title_card(title_text, bg_video=bg_video))
 
     google_search = CustomGoogleSearchAPIWrapper()
+    with st.expander("Click to expand google image search queries"):
+        for i in range(len(ranking_list), 0, -1):
+            media_query = f"{topic} {ranking_list[j][0][0]} {ranking_list[j][2][0]}"
+            st.write(media_query)
+            media_results = google_search.search_media(media_query)
+            # pic from top 5 results
+            media_file_path = f"media_{j}.jpg"
+            image_downloaded = False
 
-    for i in range(len(ranking_list), 0, -1):
-        media_query = f"{ranking_list[j]} {user_input}"
-        media_results = google_search.search_media(media_query)
-        # Random pic from top 3 results
-        media_url = random.choice(media_results[:3])["link"]
-        media_file_path = f"media_{j}.jpg"
-        image_downloaded = download_image(media_url, media_file_path)
-        if not image_downloaded:
-            media_file_path = None  # Skip using the image if the download fails
-        if bg_videos is None:
-            bg_video = None
-        else:
-            bg_video = bg_videos[j % len(bg_videos)] if bg_videos else None
-        clips.append(create_ranking_frame(
-            i, ranking_list[j], bg_video=bg_video, media_file_path=media_file_path))  # Rank and Country
-        j += 1
+            for media in media_results[:5]:
+                media_url = media["link"]
+                image_downloaded = download_image(media_url, media_file_path)
+                if image_downloaded:
+                    break
+
+            if not image_downloaded:
+                media_file_path = None  # Skip using the image if the download fails
+
+            if bg_videos is None:
+                bg_video = None
+            else:
+                bg_video = bg_videos[j % len(bg_videos)] if bg_videos else None
+            clips.append(create_ranking_frame(
+                i, ranking_list[j][0][0],ranking_list[j][1][0],ranking_list[j][2][0], bg_video=bg_video, media_file_path=media_file_path))  # Rank and Country
+            j += 1
 
     final_video = concatenate_videoclips(clips)
 
@@ -131,13 +153,33 @@ def create_video(ranking_list, user_input, bg_videos=None, bg_music=None):
 
     delete_media_files(len(ranking_list))
 
+import string
+
+def remove_non_printable_chars(s):
+    lines = s.splitlines()
+    cleaned_lines = [line for line in lines if line.strip() != "."]
+    return '\n'.join(cleaned_lines)
 
 def convert_openai_response(response):
     try:
+        print(f"Raw response: {response}")  # Print the raw response for debugging
+        response = remove_non_printable_chars(response)  # Remove non-printable characters
+        response = response.strip()
+        print(f"Response: {response}")  # Print the response for debugging
+
+        if not response:
+            raise ValueError("Empty response received")
+
         ranking_list = json.loads(response)
         return ranking_list
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as e:
+        print(f"Error: {e}")  # Print the error for debugging
         return "Error: Response is not in valid JSON format."
+    except ValueError as e:
+        print(f"Error: {e}")
+        return "Error: Empty response received."
+
+
 
 
 is_generating_video = False
@@ -151,7 +193,7 @@ def main():
     user_input = st.text_input("Enter the topic of your Top 10 tiktok")
     # Add your background video files and background music file here
     uploaded_bg_videos = st.file_uploader("Choose background video files", type=[
-                                          'mp4', 'avi', 'mov'], accept_multiple_files=True)
+                                          'mp4', 'avi', 'mov', 'gif'], accept_multiple_files=True)
     uploaded_bg_music = st.file_uploader(
         "Choose a background music file", type=['mp3', 'wav'])
 
@@ -164,52 +206,77 @@ def main():
         # wiki_summary = wiki.run(wiki_prompt)
         video_template = PromptTemplate(
             input_variables=["topic"],
-            template='Given the topic "{topic}", generate a list of the overall top 10 countries specifically related to "{topic}" ranked from 10th place to 1st place.\
-    Your response should be in an array format, like this:\
-    ["Country 10th place", "Country 9th place", "Country 8th place", "Country 7th place",\
-    "Country 6th place", "Country 5th place", "Country 4th place", "Country 3rd place", "Country 2nd place", "Country 1st place"]. Make sure your response is correct before responding"'
+            template='Given the topic "{topic}", generate a list of the overall top 10 countries good/abundant/unqiue\
+                    in specifically related to "{topic}" ranked from 10th place to 1st place.\
+                    Your response should be in an array format, like this:\
+                    ["Country 10th place", "Country 9th place", "Country 8th place", "Country 7th place",\
+                    "Country 6th place", "Country 5th place", "Country 4th place", "Country 3rd place",\
+                    "Country 2nd place", "Country 1st place"]. Make sure your response is correct before responding"'
+        )
+        video_template2 = PromptTemplate(
+            input_variables=["topic"],
+            template='Compute 10th country best in specifically related to "{topic}",\
+                    Compute 9th country best in specifically related to "{topic}",\
+                    Compute 8th country best in specifically related to "{topic}"\
+                    Compute 7th country best in specifically related to "{topic}"\
+                    Compute 6th country best in specifically related to "{topic}"\
+                    Compute 5th country best in specifically related to "{topic}"\
+                    Compute 4th country best in specifically related to "{topic}"\
+                    Compute 3rd country best in specifically related to "{topic}"\
+                    Compute 2nd country best in specifically related to "{topic}"\
+                    Compute 1st country best in specifically related to "{topic}"\
+                    Your response should be in an array format, like this:\
+                    [[["Country 10th place"], ["Why?"], ["What?"]], [["Country 9th place"], ["Why?"], ["What?"]],\
+                    [["Country 8th place"], ["Why?"], ["What?"]], [["Country 7th place"], ["Why?"], ["What?"]],\
+                    [["Country 6th place"], ["Why?"], ["What?"]], [["Country 5th place"], ["Why?"], ["What?"]],\
+                    [["Country 4th place"], ["Why?"], ["What?"]], [["Country 3rd place"], ["Why?"], ["What?"]]\
+                    [["Country 2nd place"], ["Why?"], ["What?"]], [["Country 1st place"], ["Why?"], ["What?"]]\
+                    Do not forget to put your response in a 3D array format and make Why? no more than 3 words,\
+                    What? is a fact or name related to Why? and its also very concise no more than 3 words'
         )
 
-        llm = OpenAI(temperature=0.5)
-        video_chain = LLMChain(llm=llm, prompt=video_template, verbose=True)
+        llm = OpenAI(temperature=0)
+        video_chain1 = LLMChain(llm=llm, prompt=video_template, verbose=True)
+        video_chain2 = LLMChain(llm=llm, prompt=video_template2, verbose=True)
 
-        response = video_chain.run({"topic": user_input})
+        response = video_chain2.run({"topic": user_input})
+        ranking_list = convert_openai_response(response)
         # For testing the code without making openai calls $$$
         #response = '["North Korea", "France", "China", "United Kingdom", "India", "Pakistan", "Israel", "Russia", "United States", "Iran"]'
-        st.write(response)
-        #st.write(TextClip.list('color'))
+        #st.write(response)
+        #with st.expander("Click to expand st color options"):
+        #    st.write(TextClip.list('color'))
         import tempfile
 
         def save_uploaded_file(uploaded_file):
             tfile = tempfile.NamedTemporaryFile(delete=False)
             tfile.write(uploaded_file.getvalue())
             return tfile.name
+        if isinstance(ranking_list, list):
+            temp_bg_video_paths = [save_uploaded_file(
+                video) for video in uploaded_bg_videos]
+            bg_videos = [VideoFileClip(path) for path in temp_bg_video_paths]
 
-        temp_bg_video_paths = [save_uploaded_file(
-            video) for video in uploaded_bg_videos]
-        bg_videos = [VideoFileClip(path) for path in temp_bg_video_paths]
+            temp_bg_music_path = None if uploaded_bg_music is None else save_uploaded_file(
+                uploaded_bg_music)
+            bg_music = None if temp_bg_music_path is None else temp_bg_music_path
 
-        temp_bg_music_path = None if uploaded_bg_music is None else save_uploaded_file(
-            uploaded_bg_music)
-        bg_music = None if temp_bg_music_path is None else temp_bg_music_path
+            with st.spinner("Generating video...\n this might take a minute!"):
+                # This line creates the video using the ranking list.
+                create_video(ranking_list, user_input, bg_videos, bg_music)
 
-        with st.spinner("Generating video...\n this might take a minute!"):
-            # This line creates the video using the ranking list.
-            create_video(convert_openai_response(
-                response), user_input, bg_videos, bg_music)
+            is_generating_video = False
 
-        is_generating_video = False
+            if not is_generating_video and os.path.exists("ranking_video.mp4"):
+                st.video("ranking_video.mp4")
 
-        if not is_generating_video and os.path.exists("ranking_video.mp4"):
-            st.video("ranking_video.mp4")
+            for video_clip in bg_videos:  # Close video clips before deleting the temporary files
+                video_clip.close()
+            for path in temp_bg_video_paths:
+                os.remove(path)
 
-        for video_clip in bg_videos:  # Close video clips before deleting the temporary files
-            video_clip.close()
-        for path in temp_bg_video_paths:
-            os.remove(path)
-
-        if temp_bg_music_path is not None:
-            os.remove(temp_bg_music_path)
+            if temp_bg_music_path is not None:
+                os.remove(temp_bg_music_path)
 
 
 if __name__ == "__main__":
